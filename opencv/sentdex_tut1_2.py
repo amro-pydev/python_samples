efaaa1e63b26f9b56ea7a523c5afb8f82ffbd0d2
Amr <amrsfmt@yahoo.com>
2016-05-03 17:18:27 -0700

    Adding second part of the code.

---
+++ opencv/sentdex_tut1_2.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('input_images/set10_1.jpg',cv2.IMREAD_GRAYSCALE)
+
+#IMREAD_GRAYSCALE 0
+#IMREAD_COLOR = 1
+#IMREAD_UNCHANGED = -1
+
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+
+
+
