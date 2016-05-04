adf82218fd18276f56cf2d6b39b48d7aaf17ea0f
Amr <amrsfmt@yahoo.com>
2016-05-03 17:35:32 -0700

    Minor fix.

--- opencv/sentdex_tut2_2.py
+++ opencv/sentdex_tut2_2.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
+
 
 while True:
     ret, frame = cap.read()
@@ -15,5 +18,7 @@ while True:
         break
 
 cap.release()
+out.release()
+
 cv2.destroyAllWindows()
 
